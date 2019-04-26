72c73677eed7500d4d80d2f4831f0158a8fecb32
carta <nevergreendd@github.com>
2019-04-26 13:17:44 +0900

    argument parsing added

--- run.py
+++ run.py
@@ -1,42 +1,58 @@
 import subprocess
 import os
+import agrparse
 
-# step 1
-cmd_1 = 'mpc-info -i /data1 -c 4 | grep Suggested\ Potree-OctTree\ CAABB'
-p = subprocess.Popen(cmd_1, stdout=subprocess.PIPE, shell=True)
 
-line = p.stdout.read()
+def main(args):
+    # step 1
+    cmd_1 = 'mpc-info -i /data1 -c 4 | grep Suggested\ Potree-OctTree\ CAABB'
+    p = subprocess.Popen(cmd_1, stdout=subprocess.PIPE, shell=True)
 
-lineList = line.split(',')
+    line = p.stdout.read()
 
-caabb = []
+    lineList = line.split(',')
 
-for i in range(7):
-    if i != 0:
-        caabb.append(lineList[i].strip())
+    caabb = []
 
-caabb[-1] = caabb[-1][:-1]   # remove ')'
+    for i in range(7):
+        if i != 0:
+            caabb.append(lineList[i].strip())
 
-# step 2
-tileNumber = 4
-processNumber = 4
+    caabb[-1] = caabb[-1][:-1]   # remove ')'
 
-cmd_2 = "mpc-tiling -i /data1/ -o /data2/ -t /data3/ -e \"" + caabb[0] + " " + caabb[1] + " " +  caabb[3] + " " + caabb[4] + "\"" + " -n " + str(tileNumber) + " -p " + str(processNumber)
+    # step 2
+    tileNumber = args.number_of_processes
+    processNumber = args.number_of_tiles
+
+    cmd_2 = "mpc-tiling -i /data1/ -o /data2/ -t /data3/ -e \"" + caabb[0] + " " + caabb[1] + " " +  caabb[3] + " " + caabb[4] + "\"" + " -n " + str(tileNumber) + " -p " + str(processNumber)
  
-os.system(cmd_2)
+    os.system(cmd_2)
 
-# step 3
-cmd_3 = "mpc-create-config-pycoeman -i /data2 -o /data1/ParallelPotreeConverter.xml -f LAZ -l 9 -s 83 -e \""
+    # step 3
+    cmd_3 = "mpc-create-config-pycoeman -i /data2 -o /data1/ParallelPotreeConverter.xml -f LAZ -l 9 -s 83 -e \""
 
-for i in range(len(caabb)):
-    cmd_3 += caabb[i] 
-    if i != 5:
-        cmd_3 += " "
-cmd_3 += "\""
+    for i in range(len(caabb)):
+        cmd_3 += caabb[i] 
+        if i != 5:
+            cmd_3 += " "
+    cmd_3 += "\""
 
-os.system(cmd_3)
+    os.system(cmd_3)
 
-# step 4
-cmd_4 = 'coeman-par-local -d / -c /data1/ParallelPotreeConverter.xml -e /data1/execution -n 4'
+    # step 4
+    cmd_4 = 'coeman-par-local -d / -c /data1/ParallelPotreeConverter.xml -e /data1/execution -n 4'
 
-os.system(cmd_4)
+    os.system(cmd_4)
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--number_of_processes", default=4, type=int) # default=nproc ??
+    parser.add_argument("--number_of_tiles", defulat=4, type=int)
+
+    args = parser.parse_args(None)
+    return args
+
+if __name__ == "__main__":
+    args = parse_argument()
+    main(args)
